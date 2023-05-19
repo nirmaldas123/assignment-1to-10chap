5e9b2b5b3b7bb33ef5fb6da46876f2b921bf8ab4
nirmaldas123 <mrnirmaldas456@gmail.com>
2023-05-19 16:58:27 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,274 @@
+// Chapter 1 (Alerts)
+// Q1
+// alert("First name");
+// alert("last name");
+// alert("email");
+// alert("phone number");
+// alert("password");
+// Q2
+// alert("You're learning JavaScript!");
+// Q3
+// alert("Pakistan zindabad");
+
+// Chapter 2 (Variables for string)
+// Q1
+// var myName = "nirmal";
+// console.log(myName);
+// Q2
+// var name;
+// name="dass";
+// console.log(name);
+
+// Q3
+// var teamName ="boss"
+// alert(teamName);
+
+// Q4
+// var bestMan = "Charlie";
+// bestMan = "nirmal";
+// alert(bestMan);
+// console.log(bestMan);
+
+
+
+// Chapter 3 (Variables for numbers)
+
+// Q1
+// var caseQty;
+// Q2
+// var caseQty=144;
+// alert(caseQty);
+
+// Q3
+// var num=9;
+// alert(num);
+
+// Q4
+// var num;
+// num=2+2;
+// alert(num);
+
+// Q5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal=merchTotal + shippingCharge;
+// alert(orderTotal);
+
+// Q6
+// var num=10;
+// num=10+90;
+// alert(num);
+
+// Chapter 4 (Variable names Legal and Illegal)
+
+// Q1
+// var product_cost = 3.45;
+// alert(product_cost);
+
+// Q2
+// var nameOfBand;
+// Q3
+// var legally_named="5";
+// Q4
+// var nirmalDas;
+// Q5
+// illegal
+// var $#;
+// var var;
+// var first name;
+// var 1abc;
+// var 123;
+// llegal
+// var $abc;
+// var $123;
+// var first_name;
+// var abc;
+// var ab12;
+// var aliMughal;
+
+// Chapter 5 (Math Expression I)
+// Q1
+// module operater
+// Q2
+// var num = 20 % 6;
+// alert(num);
+// answer is 2
+// Q3
+// var largeNum=2000000;
+// Q4
+// var num1=10;
+// var num2=5;
+// var num3= num1-num2; 
+// alert(num3);
+// Q5
+// var num1 = 10;
+// var num2 = 5;
+// var rem=num1/num2;
+// alert(rem);
+
+//Q6
+// var num1 = 10;
+// var num2 = 5;
+// var MUL=num1*num2;
+// alert(MUL);
+
+// Chapter 6 (Math Expression II)
+// Q1
+// var num=10;
+// num++;
+// alert(num);
+
+// Q2
+// var x=100;
+// x--;
+// alert(x);
+
+// Q3
+// var x = 50;
+// var y = x++;
+// alert(y);
+// y=50
+
+// Q4
+// var y = 50;
+// var z = --y;
+// alert(z);
+// z=49
+
+// Q5
+// var num=5;
+// newNum=num--;
+// alert(newNum);
+
+// Q6
+// var num=5;
+// newNum=num++;
+// alert(newNum);
+
+// Q7
+// var num=5;
+// newNum=++num;
+// alert(newNum);
+
+// Chapter 7 (Math Expression III)
+
+// Q1
+// var calculatedNum = 2 + (2 * 6);
+// alert(calculatedNum);
+// Answer 14
+
+// Q2
+// var calculatedNum = (2 + 2) * 6;
+// alert(calculatedNum);
+// Answer 24
+
+// Q3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// alert(calculatedNum);
+// Answer 24
+
+// Q4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// alert(calculatedNum);
+// Answer 18
+
+// Q5
+// var cost=(2 + 2) * (4 + 10);
+// alert(cost);
+
+// Q6
+// var unit=2 + 2 * 4 + 10;
+// alert(unit);
+
+
+
+// Q7
+// var pressure=4 /(2 * 4);
+// alert(pressure);
+
+// Chapter 8 (Concatenating Text Strings)
+
+// Q1
+// var num = "2" + "2";
+// alert(num);
+// answer=22;
+
+// Q2
+// var message = ("Hello," + "Dolly");
+// alert(message);
+// answer= hello,Dolly
+
+// Q3
+// alert("33" + 3);
+
+// // Q4
+// alert("Pakistan" +" "+ "Zindabad");
+
+// Q5
+// alert("Pakistan" +" "+ 456);
+
+// Q6
+// var abc = "message me "
+// var ab = "call me"
+// var xyz=abc+ab;
+// alert(xyz);
+
+// Chapter 9 (Prompts)
+
+// Q1
+// var abc = prompt("Enter first name");
+
+// Q2
+// var abc = prompt("Country?","china");
+
+// Q3
+// var yourName = prompt("Enter Your Name");
+
+// Q4
+//  var abc = prompt("Message me");
+
+ //Q5
+//  var name="NIRMAL";
+//  var name1="KAMAL";
+//  var user =prompt("NAME",name);
+//  var user =prompt("NAME2",name1);
+//  prompt("user",user)
+
+ //Q6
+//  var abc = prompt("Country?","china");
+//  alert(abc);
+
+// Chapter 10 (if statments)
+// Q1
+// var city="karachi";
+// if(city=="karachi")
+// {
+//     console.log("City of lights")
+// }
+// Q2
+// var x="abc"
+// var y="abc"
+// if (x === y) 
+// {
+// var z =prompt("Value of z")
+// }
+
+// Q3
+// var zipcode=1002003;
+// if(zipcode==1002003)
+// {
+//     alert("that is karachi");
+// }
+// else
+// {
+//     alert ("Please write correct city");
+// }
+
+// Q4
+var y=2;
+if(y==2)
+{
+    var x=1;
+var y=  x;
+console.log(y);
+}
